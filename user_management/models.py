e8b24531aac651bf1caf1129f1dcdcb6f9faa48f
gad26032 <gad26032@gmail.com>
2021-08-02 20:36:37 +0300

    step

--- user_management/models.py
+++ user_management/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 
 
-class BasicUser(User):
+class BasicUser(AbstractBaseUser):
     pass
 
